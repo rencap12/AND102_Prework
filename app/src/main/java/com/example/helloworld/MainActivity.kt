e8b161799d51965b6e8a2e5b1811b549528618c0
Reneca Capuno <capunoreneca@gmail.com>
2025-08-20 20:38:56 -0400

    done prework

---
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.helloworld
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.helloworld.ui.theme.HelloWorldTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener{
+            Log.v("Hello world", "Button clicked!")
+            Toast.makeText(this,"Hello to you too!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
+
